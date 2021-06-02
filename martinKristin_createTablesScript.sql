6896e617f4d42b07242964c7ed8f4df45a53ca23
Kristin Martin <33077202+kristinvmartin@users.noreply.github.com>
2021-06-01 21:55:07 -0400

    Update martinKristin_createTablesScript.sql

--- martinKristin_createTablesScript.sql
+++ martinKristin_createTablesScript.sql
@@ -1,3 +1,9 @@
+/*
+This script is to create the schema for the warehouse. It is to be loaded in MSSQL Server before running
+any of the .ipynb files.
+*/
+
+--drop tables/constraints/sequences to ensure error-free runs during testing
 DROP SEQUENCE IF EXISTS dateTime_PK;
 DROP SEQUENCE IF EXISTS location_PK;
 DROP SEQUENCE IF EXISTS disaster_PK;
@@ -9,6 +15,7 @@ DROP TABLE IF EXISTS Location_Dimension;
 DROP TABLE IF EXISTS Disaster_Dimension;
 DROP TABLE IF EXISTS Relief_Fact;
 
+--creating the date/time dimension, used granularity as specified by business questions and to account for any flexibility needed
 CREATE TABLE DateTime_Dimension (
 	dateTimeId decimal PRIMARY KEY,
 	calendarDate date NOT NULL,
@@ -21,10 +28,11 @@ CREATE TABLE DateTime_Dimension (
 	fiscalYear int NOT NULL
 );
 
+--creating the location dimension. Did not normalize zip codes, states, etc to avoid overnormalizing and improve processing speed of queries
 CREATE TABLE Location_Dimension (
 	locationId decimal PRIMARY KEY,
 	locRegion varchar(2),
-	locPriorRegion varchar(2) DEFAULT NULL,
+	locPriorRegion varchar(2) DEFAULT NULL, --in case the location has been re-assigned to a different region, this mitigates data loss.
 	locState varchar (40) NOT NULL,
 	locCounty varchar(255) NOT NULL,
 	locCity varchar(255) NOT NULL,
@@ -32,36 +40,39 @@ CREATE TABLE Location_Dimension (
 	locationIndex decimal NOT NULL
 );
 
+--creating the disaster dimension
 CREATE TABLE Disaster_Dimension (
 	disasterId decimal PRIMARY KEY,
-	femaDisasterNumber decimal NOT NULL,
-	incidentType varchar(40) NOT NULL,
-	designatedArea varchar(255) NOT NULL,
-	rowEffectiveDate date NOT NULL DEFAULT GETDATE(),
-	rowExpirationDate date DEFAULT NULL,
-	currentIndicator BIT NOT NULL DEFAULT 1
+	femaDisasterNumber decimal NOT NULL, --number designated by FEMA
+	incidentType varchar(40) NOT NULL, --hurricane, flood, etc.
+	designatedArea varchar(255) NOT NULL, --main area designated for disaster (FEMA provides)
+	rowEffectiveDate date NOT NULL DEFAULT GETDATE(), --to ensure most current data is in table without creating duplicate records on periodic updates
+	rowExpirationDate date DEFAULT NULL, --same as rowEffectiveDate
+	currentIndicator BIT NOT NULL DEFAULT 1 --same as above
 );
 
+--creating the Relief fact table to answer key business questions, IDs are FKs to dimension tables
 CREATE TABLE Relief_Fact (
 	disasterId decimal,
 	locationId decimal,
 	dateTimeId decimal,
-	totalValidRegistrations int NOT NULL,
-	ihpReferrals int NOT NULL,
-	ihpEligible int NOT NULL,
-	ihpAmount decimal (20,2) NOT NULL,
-	haReferrals int NOT NULL,
-	haEligible int NOT NULL,
-	haAmount decimal (20,2) NOT NULL,
-	onaReferrals int NOT NULL,
-	onaEligible int NOT NULL,
-	onaAmount decimal (20,2) NOT NULL
+	totalValidRegistrations int NOT NULL, --how many people registered for aid
+	ihpReferrals int NOT NULL, --how many people referred
+	ihpEligible int NOT NULL, --how many registrants found eligible
+	ihpAmount decimal (20,2) NOT NULL, --how much IHP aid received
+	haReferrals int NOT NULL, --how many housing assistance referrals received
+	haEligible int NOT NULL, --how many found eligible
+	haAmount decimal (20,2) NOT NULL, --how much HA aid received
+	onaReferrals int NOT NULL, --total IHP + HA referrals
+	onaEligible int NOT NULL, --total IHP + HA eligible applicants
+	onaAmount decimal (20,2) NOT NULL --total aid received
 );
-
+--adding FK constraints
 ALTER TABLE Relief_Fact ADD CONSTRAINT disaster_FK FOREIGN KEY (disasterId) REFERENCES Disaster_Dimension(disasterId);
 ALTER TABLE Relief_Fact ADD CONSTRAINT location_FK FOREIGN KEY (locationId) REFERENCES Location_Dimension(locationId);
 ALTER TABLE Relief_Fact ADD CONSTRAINT dateTime_FK FOREIGN KEY (dateTimeId) REFERENCES DateTime_Dimension(dateTimeId);
 
+--creating sequences to automate PK creation
 CREATE SEQUENCE dateTime_PK
 AS decimal
 START WITH 1000000
